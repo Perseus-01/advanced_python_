c7fa35519d295aaca97e3fec968028d83feb9329
Alexey Chizhevskiy <aleksei.chizhevskii@gmail.com>
2023-04-09 20:20:08 +0400

    made task 1 and 2

--- REGULAR EXPRESSIONS/regex.py
+++ REGULAR EXPRESSIONS/regex.py
@@ -1,47 +1,58 @@
-
 from pprint import pprint
+
 ## Читаем адресную книгу в формате CSV в список contacts_list:
 import csv
 import re
-with open("phonebook_raw.csv", encoding='utf-8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
 
-
-
-# 1. # Поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно. 
+# 1. # Поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно.
 # В записной книжке изначально может быть Ф + ИО, ФИО, а может быть сразу правильно: Ф+И+О.
-# По первому пункту можно сначала соединить содержимое первых трех элементов и разбить по пробелам, 
+# По первому пункту можно сначала соединить содержимое первых трех элементов и разбить по пробелам,
 # вы получите список 2-3 элементов. Его интегрируете в запись.
 
-for item in contacts_list:
-  for i in item:
-    fio = item[0:2]
-    print(item) 
+def fix_columns(some_list):
+    for row in contacts_list:
+        temp = ''.join(row[:3])
+        temp_list = temp.split()
+        for i in range(len(temp_list)):
+            row[i] = temp_list[i]
+    return some_list
 
+fix_columns(contacts_list)
 
-#2 # Привести все телефоны в формат +7(999)999-99-99. 
+# 2 # Привести все телефоны в формат +7(999)999-99-99.
 # Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999.
 
+def format_number(some_list):
+  current_pattern = r'(\+7|8)\D*(495)\D*(\d+)\D*(\d{2})\D*(\d{2})(\W*( доб.\s*\d+)\)*)?'
+  replace_pattern = r'+7(\2)\3-\4-\5\7'
+  for row in some_list:
+    result = re.sub(current_pattern, replace_pattern, row[-2])
+    row[-2] = result
 
-# def format_number(contacts_list):
-#   phone_pattern = r'(\+7|8)\D*(495)\D*(\d+)\D*(\d{2})\D*(\d{2})(\W*( доб.\s*\d+)\)*)?'
-#   replace = r'+7(\2)\3-\4-\5\7'
-#   contacts_list_updated = list()
-#   for card in contacts_list:
+  return some_list
 
-#3 # Объединить все дублирующиеся записи о человеке в одну.
+format_number(contacts_list)
 
 
+# 3 # Объединить все дублирующиеся записи о человеке в одну.
 
-## 1. Выполните пункты 1-3 задания:
-## Ваш код
+# 3й пункт лучше делать через словарь. 
+# Ключом делайте ФИ и при добавлении очередного элемента (записи) делайте проверку на наличие ключа в словаре. 
+# Если ключ есть, то нужно объединить 2 записи, если нет, то добавляете новую в словарь.
 
-## 2. Сохраните получившиеся данные в другой файл.
+def merge_doubles(some_list):
+   
+
+
+## Сохраните получившиеся данные в другой файл.
 ## Код для записи файла в формате CSV:
 # with open("phonebook.csv", "w") as f:
 #   datawriter = csv.writer(f, delimiter=',')
-  
+
 ## Вместо contacts_list подставьте свой список:
 #   datawriter.writerows(contacts_list)
